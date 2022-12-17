0f984090e0a42e9ea3e5dbefe96ccc65ddc30436
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-12-17 16:29:09 +0500

    Fetched the prices of the crypto currencies from messari.io

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const socketIO = require("socket.io");
+const axios = require("axios");
 const PORT = 8000;
 
 const server = express().listen(PORT, () => {
@@ -10,8 +11,18 @@ const socketHandler = socketIO(server);
 
 socketHandler.on("connection", () => {
   console.log("Connected to the client.");
-    socketHandler.emit("crypto", {
-        message: "Success connected to the client.",
-      });
-
+  socketHandler.emit("crypto", {
+    message: "Success connected to the client.",
+  });
 });
+
+axios
+  .get(
+    "https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd"
+  )
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
